e2f177b100799ef1829464a443cb45ca8a0bdf92
ogumaru <100858200+ogumaru@users.noreply.github.com>
2022-03-20 21:15:48 +0900

    update: implement creating api

--- pages/api/liking.ts
+++ pages/api/liking.ts
@@ -11,16 +11,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ILikingItemClient[]>
 ) {
-  switch (req.method?.toUpperCase()) {
-    case "GET": {
-      const records = await getRecords();
-      res.status(200).json(records);
-    }
-    case "POST": {
-      if (isNewRecord(req.body)) {
-        const result = await setRecord(req.body);
-        res.status(200);
+  try {
+    switch (req.method?.toUpperCase()) {
+      case "GET": {
+        const records = await getRecords();
+        res.status(200).json(records);
+      }
+      case "POST": {
+        const parsed = JSON.parse(req.body);
+        if (isNewRecord(parsed)) {
+          const result = await setRecord(parsed);
+          res.status(200);
+        }
+      }
+      default: {
+        res.status(500);
       }
     }
+  } catch {
+    res.status(500);
   }
 }
